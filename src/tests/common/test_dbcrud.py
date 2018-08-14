7d234006292bde41b8c8272ed78078cd6fb203ef
thuanghai <t_huanghai@qq.com>
2018-08-14 10:23:48 +0800

    fix argument for dbcrund module changes.

--- src/tests/common/test_dbcrud.py
+++ src/tests/common/test_dbcrud.py
@@ -21,36 +21,32 @@ def test_db_connect():
     """
     Test mongodb connect
     """
-    # chkmongo_cfg = {'host':'0.0.0.0','port':27017}
-    chkmongo_cfg = (
-        '0.0.0.0',
-        27017
-    )
-    chkdb_name = 'local'
-    chkdb = dbcrud.db_connect(chkmongo_cfg, chkdb_name)
-    assert chkdb.name == chkdb_name
+    chk_db_host = '0.0.0.0'
+    chk_db_port = 27017
+    chk_db_name = 'local'
+    chkdb = dbcrud.db_connect(chk_db_host, chk_db_port, chk_db_name)
+    assert chkdb.name == chk_db_name
 
 def test_create_one():
     """
     Test insert and find one document in mongodb
     """
-    mongo_cfg = (
-        '0.0.0.0',
-        27017
-    )
-    mongo_db = 'dbcrud_test_db'
-    mongo_collection = 'dbcrud_test_collection'
+    db_host = '0.0.0.0'
+    db_port = 27017
+    db_name = 'dbcrud_test_db'
+    db_collection = 'dbcrud_test_collection'
     insert_data_id = TEST_ID
-    mongo_insert_document = {
+    insert_document = {
         '_id':insert_data_id, 
         'name':'test_doc', 
         'description':'A test document in mongodb'
         }
     chkresult = dbcrud.create_one(
-        mongo_cfg, 
-        mongo_db, 
-        mongo_collection, 
-        mongo_insert_document
+        db_host, 
+        db_port,
+        db_name, 
+        db_collection, 
+        insert_document
         )
     assert chkresult == insert_data_id
 
@@ -58,18 +54,16 @@ def test_delete_one():
     """
     Test delete one document in mongodb
     """
-    mongo_cfg = (
-        '0.0.0.0',
-        27017
-    )
-    mongo_db = 'dbcrud_test_db'
-    mongo_collection = 'dbcrud_test_collection'
-    # Note: Delete document using 'test_create_one' result
+    db_host = '0.0.0.0'
+    db_port = 27017
+    db_name = 'dbcrud_test_db'
+    db_collection = 'dbcrud_test_collection'
     delete_data_id = TEST_ID
     chkresult = dbcrud.delete_one(
-        mongo_cfg,
-        mongo_db,
-        mongo_collection,
+        db_host, 
+        db_port,
+        db_name, 
+        db_collection, 
         delete_data_id
     )
     assert chkresult == 1
